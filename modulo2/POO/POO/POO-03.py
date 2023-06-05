968d7cbac9460261b7239643d21f44373a3b7f78
Danylo Miranda <danylo.s.miranda@gmail.com>
2023-06-05 19:22:09 -0300

    Aula 16
    códigos desenvolvidos durante a aula 16 modulo 2

--- modulo2/POO/POO/POO-03.py
+++ modulo2/POO/POO/POO-03.py
@@ -1,14 +1,14 @@
 class Carro():
     cor = 'amarela'
        
-    def __init__(self,rodas,tanque): #inicia o metodo pré-modulado
-        self.rodas = rodas
+    def __init__(self,rodas,tanque): #metodo construtor inicia os atributos junto com classe
+        self.rodas = rodas 
         self.litrostanque = tanque
     
-    def get_cor(self): #retorna valores
+    def get_cor(self): #'get_' retorna valores
         return self.cor   
     
-    def set_cor(self,novacor): #modifica valores
+    def set_cor(self,novacor): # 'set_' modifica valores
         self.cor = novacor
 
 carrim = Carro(4,50)
---
+++ modulo2/POO/POO/livecoding-01.py
@@ -0,0 +1,34 @@
+class Funcionario():
+    def __init__ (self, nome, salario_base):
+        self.nome = nome
+        self.salario_base = salario_base
+        
+    def calcularSalario(self):
+        pass
+        
+
+class Funcionarioclt(Funcionario):
+    def tempo_de_empresa(self, tempo):
+        self.tempo = tempo
+        
+    def calcularSalario(self):
+        return self.salario_base * (self.tempo / 100)
+
+class Funcionariopj(Funcionario):
+    def experiencia (self,experiencia):
+        self.experiencia = experiencia
+
+    def calcularSalario(self):
+        return self.salario_base * self.experiencia
+
+funcionarioCLT1 = Funcionarioclt('Fernando', 1300)
+funcionarioCLT1.tempo = 12
+funcionarioCLT1.calcularSalario()
+
+print (f'O salario do funcionario {funcionarioCLT1.nome} é R${funcionarioCLT1.calcularSalario():.2f}')
+
+funcionarioPJ1 = Funcionariopj('Josean', 1300)
+funcionarioPJ1.experiencia = 2
+funcionarioPJ1.calcularSalario()
+
+print (f'O salario do funcionario {funcionarioPJ1.nome} é R${funcionarioPJ1.calcularSalario():.2f}')
